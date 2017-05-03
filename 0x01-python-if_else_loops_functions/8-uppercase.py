d33b52bda21b00b35907da216ca9518a4f70838d
Bobby Yang <122@holbertonschool.com>
2017-05-03 01:21:30 +0000

    try naomi

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
+        if 97 <= ord(i) <= 122:
             char = chr(ord(char) - 32)
         print("{:s}".format(char), end="")
-    print('\n', end="")
+    print('')

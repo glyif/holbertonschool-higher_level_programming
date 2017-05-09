e42fbf4b09fe3c2e05439922d66adea85608c809
Bobby Yang <122@holbertonschool.com>
2017-05-09 20:46:57 +0000

    updated with task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-def print_list(matrix_list=[]):
-    for n in range(0, len(matrix_list)):
-        if n < len(matrix_list) - 1:
-            print("{:d}".format(matrix_list[n]), end=" ")
-        else:
-            print("{:d}".format(matrix_list[n]))
-
-
 def print_matrix_integer(matrix=[[]]):
-    if(len(matrix) == 0 or matrix is None):
-        return matrix
-
+    string = ''
     for list in matrix:
-        i = 0
-        new_list = []
-        for char in list:
-            new_list.append(char)
-            length = len(new_list)
-        print_list(new_list)
+        string += "\n"
+        for element in row:
+            string += "{:d} ".format(element)
+        string = string[:-1]
+
+    print (string[1:])

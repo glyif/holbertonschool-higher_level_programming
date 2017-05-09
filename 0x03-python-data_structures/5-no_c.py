17753e47889c5e2f016c4b537db5f647d648f89e
Bobby Yang <122@holbertonschool.com>
2017-05-09 20:40:13 +0000

    try 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string_list = list(my_string)
-    i = 0
-    for element in string_list:
-        if element == 'c' or element == 'C':
-            del string_list[i]
-            i = i + 1
-    return (''.join(string_list))
+    return (''.join(c for c in my_string if c not in 'cC'))

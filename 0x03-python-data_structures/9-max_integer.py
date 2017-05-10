0e815bb1757a9eeefd7a603a1dce60675b29a82f
Bobby Yang <122@holbertonschool.com>
2017-05-10 01:13:58 +0000

    updated with initial value of max_value for task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) is 0 or my_list is None:
-        return
+        return(None)
 
-    max_value = 0
+    max_value = my_list[0]
     for value in my_list:
-        if value > max_value:
+        if value >= max_value:
             max_value = value
 
-    return (max_value)
+    return(max_value)

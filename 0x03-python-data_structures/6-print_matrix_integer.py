ce51ae4b4aa7407cba29bfdcd934047b89ab5f3f
Bobby Yang <122@holbertonschool.com>
2017-05-10 20:04:02 +0000

    updated with join method for number 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    string = ''
     for lists in matrix:
-        string += "\n"
-        for element in lists:
-            string += "{:d} ".format(element)
-        string = string[:-1]
-
-    print(string[1:])
+        print(" ".join("{:d}".format(element) for element in lists))

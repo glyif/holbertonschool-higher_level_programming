96cdf4d07382e8f24ac64ada14d2bdba7a2d4452
Bobby Yang <122@holbertonschool.com>
2017-05-11 23:25:01 +0000

    updated without key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(my_dict):
     if my_dict is None or len(my_dict) == 0:
         return(None)
-    sorted_dict = sorted(my_dict, key=lambda score: score[1])
+    sorted_dict = sorted(my_dict)
     return(sorted_dict[-1])

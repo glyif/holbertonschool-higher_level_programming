d7c058cdc6b2e3c6ccea7586c576604a55b28b91
Bobby Yang <122@holbertonschool.com>
2017-06-03 23:54:50 +0000

    udpated with task 8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file - creates an Object from a "JSON file"
+
+    Args:
+    @filename: JSON file to read from
+    """
+    with open(filename, mode='r') as fd:
+        json_obj = f.read()
+        return (json.loads(json_obj))

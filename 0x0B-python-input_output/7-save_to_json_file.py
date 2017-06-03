53ad21c11dbb38023e1e24333e456b2aa1921f28
Bobby Yang <122@holbertonschool.com>
2017-06-03 23:49:40 +0000

    udpated with task 7

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an Object to a text file, using a JSON representation
+    
+    Args:
+    @my_obj: object to a text file using json
+    @filename: name of while to write in
+    """
+    with open(filename, mode='w') as fd:
+        fd.write(json.dumps(my_obj))

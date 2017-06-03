5780f1636aeda8957e9e9823d43c9aa7579a85a6
Bobby Yang <122@holbertonschool.com>
2017-06-03 23:50:34 +0000

    updated with pep8 for number 7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """
     writes an Object to a text file, using a JSON representation
-    
+
     Args:
     @my_obj: object to a text file using json
     @filename: name of while to write in

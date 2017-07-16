75269f63cfdbfd9d308a3c90c3a4b58839484a14
Bobby Yang <122@holbertonschool.com>
2017-07-16 18:55:42 +0000

    used columns

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- subquery
 
-SELECT * FROM cities WHERE state_id = (
-	SELECT * FROM states WHERE name = 'California')
+SELECT id, names FROM cities WHERE state_id = (
+	SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;

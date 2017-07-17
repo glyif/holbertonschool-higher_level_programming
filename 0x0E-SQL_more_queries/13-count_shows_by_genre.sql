a35e0e7cd1ade303978b39a7c3cc225d3c5efb70
Bobby Yang <122@holbertonschool.com>
2017-07-17 00:43:49 +0000

    task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- number of shows per genre
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+	FROM tv_genres INNER JOIN tv_show_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	ORDER BY number_shows DESC;

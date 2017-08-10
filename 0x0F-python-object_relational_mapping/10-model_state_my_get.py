fecdd2ca2ec91d48f37de26adb2efb5570a637a3
Bobby Yang <122@holbertonschool.com>
2017-08-10 19:49:54 +0000

    commit all mandatory tasks

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{:s}:{:s}@localhost/{:s}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    data = session.query(State).filter(State.name == sys.argv[4].split(' ')[0].split(';')[0]).first()
+    print(data.id)
---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{:s}:{:s}@localhost/{:s}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    data = session.query(State).filter(State.id == 2).first()
+    if data:
+        data.name = "New Mexico"
+        session.commit()

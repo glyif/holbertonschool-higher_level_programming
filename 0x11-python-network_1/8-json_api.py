911af0c4dc83a289c8714de3ea4f7bd7bcee5acd
glyif <122@holbertonschool.com>
2017-08-28 17:33:04 -0700

    fixed #8 bugs

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,18 +8,18 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv) == 3:
-        parameter = argv[2]
+    if len(argv) == 2:
+        parameter = argv[1]
     else:
         parameter = ""
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': argv[1]})
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': parameter})
 
     try:
         response = r.json()
-        if len(reponse.keys() == 0):
+        if len(response.keys()) == 0:
             print("No result")
         else:
-            print("[{}] {}".format(reponse.get("id"), response.get("name")))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except:
         print("Not a valid JSON")

3bd98edb56056ba7148dd656cb82c58b85ace67d
glyif <122@holbertonschool.com>
2017-09-25 15:06:30 -0700

    updated with Square class inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 // square class
 
-const Rectangle = require('./4-rectangle').Rectangle;
+const Square = require('./5-square').Square;
 
-function Square (size) {
-  Rectangle.call(this, size, size);
-
-  this.charPrint = function (char) {
-    if (char === undefined) {
-      char = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
-    }
-  };
-}
+Square.prototype.charPrint = function (char) {
+  if (char === undefined) {
+    char = 'X';
+  }
+  for (let i = 0; i < this.height; i++) {
+    console.log(char.repeat(this.width));
+  }
+};
 
 module.exports = {Square};

fc73d137db3d7d3bb806aa1b491b6ba9e76cd6d3
glyif <122@holbertonschool.com>
2017-09-25 14:49:03 -0700

    moved to inside if statement

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,13 +5,12 @@ function Rectangle (w, h) {
   if (w > 0 && h > 0) {
     this.width = w;
     this.height = h;
+    this.print = function () {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      };
+    }
   }
 }
-
-Rectangle.prototype.print = function () {
-  for (let i = 0; i < this.height; i++) {
-    console.log('X'.repeat(this.width));
-  }
-};
 
 module.exports = {Rectangle};
